79e57f7befb9ab9320e2b64dc6a4f32461ebe781
Nouwatin Jacob <jaysansa@gmail.com>
2018-02-25 20:08:44 +0100

    Topic view with its replies

---
+++ resources/views/topics/show.blade.php
@@ -0,0 +1,81 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                   <img src="{{ asset($topic->user->avatar) }}" alt="{{ $topic->user->avatar }}" width="35px" height="35px">
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                    <a href="" class="text-muted">{{ $topic->title }}</a>
+                </div>
+
+                <div class="card-body">
+                    {{ $topic->description }}
+                </div>
+                <div class="card-footer">
+                <i class="fas fa-thumbs-up text-success"> 20</i>  &nbsp;&nbsp;
+                <i class="fas fa-thumbs-down text-danger"> 5</i>  &nbsp;&nbsp;
+                <i class="fas fa-clock text-muted"> Posted on: {{ $topic->created_at->diffForHumans() }}</i>
+                </div>
+            </div>
+          <br>
+          <h3 class="">Reply</h3>  
+            @foreach($replies as $reply)
+            <div class="card">
+                <div class="card-header">
+                   <img src="{{ asset($reply->user->avatar) }}" alt="{{ $reply->user->avatar }}" width="35px" height="35px">
+                </div>
+
+                <div class="card-body">
+                    {{ $reply->content }}
+                </div>
+                <div class="card-footer">
+                <i class="fas fa-thumbs-up text-success"> 10</i>  &nbsp;&nbsp;
+                <i class="fas fa-thumbs-down text-danger"> 3</i>  &nbsp;&nbsp;
+                <i class="fas fa-clock text-muted"> Posted on: {{ $reply->created_at->diffForHumans() }}</i>
+                </div>
+            </div>
+            <br>
+            @endforeach
+
+            <div class="card">
+                <div class="card-header">
+                    Post a Reply
+                </div>
+                <div class="card-body">
+                    <form action="{{ route('reply.store', ['id' => $topic->id])}}" method="POST">
+                        {{csrf_field()}}
+                        <div class="form-group">                            
+                            <textarea cols="20" rows="5" class="form-control" name="content" id="content" placeholder="Type your message here"></textarea>
+                        </div>
+
+                      <div class="form-group">
+                        <button class="btn btn-success float-right" type="submit">Post Reply</button>
+                      </div>
+                    </form>
+                </div>
+            </div>    
+
+        </div>
+        
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">Category</div>
+
+                <div class="card-body">
+                    <ul class="list-group">
+                        @foreach($categories as $category)
+                            <li class="list-group-item">
+                                {{ $category->name }}
+                                <span class="badge badge-secondary pull-right">{{ $category->topics->count()}}</span>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
