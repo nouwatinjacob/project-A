267565ec659b1a5fa267a1ca59557fe9a141027c
Nouwatin Jacob <jaysansa@gmail.com>
2018-02-27 21:48:55 +0100

    reform how to save poll

--- resources/views/polls/create.blade.php
+++ resources/views/polls/create.blade.php
@@ -20,23 +20,7 @@
                         <label for="title">Poll Title</label>                       
                        <input type="text" class="form-control" name="title" placeholder="Enter poll title">
                      </div>
-                     <div class="form-group">
-                     <label for="poll-item">Poll Items</label> 
-                     <div class="input-group control-group after-add-more">                      
-                      <input type="text" name="addmore[]" id="poll-item" class="form-control" placeholder="Enter Poll Item">
-                      <div class="input-group-btn"> 
-                        <button class="btn btn-success add-more" type="button"><i class="fa fa-plus"></i></button>
-                      </div>
-                    </div>
-                    </div>
-                    <div class="copy hide">
-                      <div class="control-group input-group" style="margin-top:10px">
-                        <input type="text" name="addmore[]" class="form-control" placeholder="Enter Other Poll Item">
-                        <div class="input-group-btn"> 
-                          <button class="btn btn-danger remove" type="button"><i class="fa fa-minus"></i></button>
-                        </div>
-                      </div>
-                    </div><br>
+                     
                      <div class="form-group">
                         <label for="title">Start Date</label>                       
                        <input type="date" class="form-control" name="start_date" >
