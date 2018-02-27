124d9002eee42344a776367f1bc8e7c9dc38c53c
Nouwatin Jacob <jaysansa@gmail.com>
2018-02-27 14:16:09 +0100

    formatted poll form

--- resources/views/includes/admin_sidebar.blade.php
+++ resources/views/includes/admin_sidebar.blade.php
@@ -3,7 +3,7 @@
     <ul class="list-group">
       <li class="list-group-item"><a href="{{ route('categories')}}">Category</a></li>
       <li class="list-group-item"><a href="{{ route('categories.create')}}">Create a new Category</a></li>
-      <li class="list-group-item"><a href="{{ route('polls.index') }}">Create new Poll</a></li>
+      <li class="list-group-item"><a href="{{ route('polls.create') }}">Create new Poll</a></li>
     </ul>
   </div>
 </div>
